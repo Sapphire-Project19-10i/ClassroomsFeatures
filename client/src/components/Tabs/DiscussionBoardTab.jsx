9316d8f9ba504fa849b103bceda246015913eed8
Radoslav Savenkov <rado9786@gmail.com>
2023-12-07 13:24:13 -0500

    Discussion Board Posts Update Frontend

--- client/src/components/Tabs/DiscussionBoardTab.jsx
+++ client/src/components/Tabs/DiscussionBoardTab.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import TextEditor from '../TextEditor';
 import { Modal, Form, Input, Button, Table, Popconfirm, message } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { createDiscussionBoard, getDiscussionBoard, getDiscussionBoards, deleteDiscussionBoard } from '../../Utils/requests';
+import { createDiscussionBoard, getDiscussionBoard, getDiscussionBoards, deleteDiscussionBoard, createDiscussionPost, getDiscussionPosts, getDiscussionPost } from '../../Utils/requests';
 
 
 export default function DiscussionBoardTab({searchParams, setSearchParams, classroomId}){
+  const [ID, setID] = useState(0);
   const [discussionBoardList, setDiscussionBoardList] = useState([]);
+  const [discussionPostList, setDiscussionPostList] = useState([]);
   const [tab, setTab] = useState(
     searchParams.has('tab') ? searchParams.get('tab') : 'home'
   );
@@ -15,8 +17,12 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
   );
   
   const [openState, setOpenState] = useState(false);
+  const [openState1, setOpenState1] = useState(false);
+  const [openState2, setOpenState2] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [content, setContent] = useState("");
+  const [name, setName] = useState("");
 
 
   useEffect(() => {
@@ -28,6 +34,21 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
       }
       else{
         wsResponse = await getDiscussionBoards();
+      }
+    };
+    fetchData();
+  }, [classroomId]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      let wsResponse;
+      if(classroomId){
+        wsResponse = await getDiscussionPost(ID);
+
+        setDiscussionPostList(wsResponse.data);
+      }
+      else{
+        wsResponse = await getDiscussionPosts();
       }
     };
     fetchData();
@@ -47,14 +68,34 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
     }
   }
 
-    
+  const handleCreateDiscussionPost = async () => {
+    let wsResponse = await createDiscussionPost(name, content, ID);
+    if(wsResponse.err){
+      message.error("Fail to create discussion post");
+    }
+    else{
+      message.success("Successfully created discussion post");
+      wsResponse = await getDiscussionPost(ID);
+      setDiscussionPostList(wsResponse.data);
+      setOpenState1(false);
+    }
+  }
+  function tempfunc(id){
+    setOpenState1(true);
+    setID(id);
+  }
+  function tempfunc2(id){
+    setOpenState2(!openState2);
+    setID(id);
+  }
+
   const wsColumn = [
       {
         title: 'Title',
         dataIndex: 'title',
         key: 'title',
         editable: true,
-        width: '30%',
+        width: '20%',
         align: 'left',
         render: (_, key) => key.title,
       },
@@ -63,9 +104,29 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
         dataIndex: 'description',
         key: 'description',
         editable: true,
-        width: '40%',
+        width: '30%',
         align: 'left',
         render: (_, key) => key.description,
+      },
+      {
+        title: 'Reply',
+        dataIndex: 'reply',
+        key: 'reply',
+        width: '10%',
+        align: 'right',
+        render: (_, key) => (
+            <button id={'link-btn'} onClick = {() => tempfunc(key.id)} >Reply</button>
+        ),
+      },
+      {
+        title: 'See posts',
+        dataIndex: 'See posts',
+        key: 'See posts',
+        width: '10%',
+        align: 'right',
+        render: (_, key) => (
+            <button id={'link-btn'} onClick = {() => tempfunc2(key.id)} >See posts</button>
+        ),
       },
       {
         title: 'Created Time',
@@ -105,7 +166,64 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
         ),
       },
   ];
-
+  
+  const postColumn = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      editable: true,
+      width: '20%',
+      align: 'left',
+      render: (_, key) => key.name,
+    },
+    {
+      title: 'Post',
+      dataIndex: 'post',
+      key: 'post',
+      editable: true,
+      width: '30%',
+      align: 'left',
+      render: (_, key) => key.post,
+    },
+    {
+      title: 'Created Time',
+      dataIndex: 'created_at',
+      key: 'created_at',
+      editable: false,
+      width: '20%',
+      align: 'left',
+      render: (_, key) => key.time,
+    },
+    {
+      title: 'Delete',
+      dataIndex: 'delete',
+      key: 'delete',
+      width: '10%',
+      align: 'right',
+      render: (_, key) => (
+        <Popconfirm
+          title={'Are you sure you want to delete this discussion board?'}
+          icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+          onConfirm={async () => {
+            const res = await deleteDiscussionPost(key.id);
+            if (res.err) {
+              message.error(res.err);
+            } else {
+              setDiscussionPostList(
+                discussionPostList.filter((db) => {
+                  return db.id !== key.id;
+                })
+              );
+              message.success('Delete success');
+            }
+          }}
+        >
+          <button id={'link-btn'}>Delete</button>
+        </Popconfirm>
+      ),
+    },
+];
 
   return (
       <div>
@@ -115,12 +233,13 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
               Create Discussion Board
             </button>
           </div>
-          <h1>Disscussion Boards</h1>
+          <h1>Discussion Boards</h1>
         </div>
         <div
           id='content-creator-table-container'
           style={{ marginTop: '6.6vh' }}
         >
+          
           <Table
             columns={wsColumn}
             dataSource={discussionBoardList}
@@ -130,16 +249,27 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
               setPage(Pagination.current);
               setSearchParams({ tab, page: Pagination.current });
             }}
+            
             pagination={{ current: page ? page : 1 }}
           ></Table>
+          {openState2 && <Table
+            columns={postColumn}
+            dataSource={discussionPostList}
+            rowClassName='editable-row'
+            rowKey='id'
+            onChange={(Pagination) => {
+              setPage(Pagination.current);
+              setSearchParams({ tab, page: Pagination.current });
+            }} 
+            
+            pagination={{ current: page ? page : 1 }}
+          ></Table> }
         </div>
-        
-        {openState &&      
           <Modal
           title="Create Discussion"
           open={openState}
           width="35vw"
-          onCancel={() => {setOpenState(false);}}
+          onCancel={() => {setOpenState(false)}}
           footer={null}
           >
             <Form
@@ -188,7 +318,63 @@ export default function DiscussionBoardTab({searchParams, setSearchParams, class
                 </Button>
               </Form.Item>
             </Form>
-          </Modal>}
+          </Modal>
+          
+          <Modal
+          title="Make Post"
+          open={openState1}
+          width="35vw"
+          onCancel={() => {setOpenState1(false);}}
+          footer={null}
+          >
+            <Form
+              id="add-units"
+              labelCol={{
+                span: 6,
+              }}
+              wrapperCol={{
+                span: 14,
+              }}
+              onFinish={handleCreateDiscussionPost}
+              layout="horizontal"
+              size="default"
+            >
+              <Form.Item id="form-label" label="Name">
+                <Input
+                  onChange={e => setName(e.target.value)}
+                  value={name}
+                  placeholder="Enter your name"
+                  required
+                />
+              </Form.Item>
+              <Form.Item id="form-label" label="Reply">
+                <Input.TextArea
+                  rows={3}
+                  onChange={e => setContent(e.target.value)}
+                  value={content}
+                  placeholder="Enter reply here"
+                  required
+                />
+              </Form.Item>
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+                style={{ marginBottom: "0px" }}
+              >
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  size="large"
+                  className="content-creator-button"
+                >
+                  Post
+                </Button>
+              </Form.Item>
+            </Form>
+          </Modal> 
+          
       </div>
   )
 }
